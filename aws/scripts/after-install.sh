1eb134e0632a3f1436dd18da8c5d357b9eb77e81
Bharath_Paravastu <Vinodnimmala12#>
2022-07-01 10:55:47 -0500

    Test

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,8 +3,8 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://##awsgithubactions##/GitHubactionsDemo-0.0.1-SNAPSHOT.jar /usr/local/tomcat9/webapps/GitHubactionsDemo-0.0.1-SNAPSHOT.jar
 
-
+aws s3 cp target/*.jar s3://${{ env.S3BUCKET }}
 # Ensure the ownership permissions are correct.
 chown -R tomcat:tomcat /usr/local/tomcat9/webapps
