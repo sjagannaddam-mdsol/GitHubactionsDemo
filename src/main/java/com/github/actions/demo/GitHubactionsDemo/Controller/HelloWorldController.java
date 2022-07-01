ff7b694ede39f0d7fedf54ac610c5bdfc0cdb1bc
Bharath_Paravastu <Vinodnimmala12#>
2022-07-01 15:05:13 -0500

    Test

--- src/main/java/com/github/actions/demo/GitHubactionsDemo/Controller/HelloWorldController.java
+++ src/main/java/com/github/actions/demo/GitHubactionsDemo/Controller/HelloWorldController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController 
 {
-@RequestMapping("/")
+@RequestMapping("/HelloApp")
 public String hello() 
 {
 return "<h1> Congratulations. You have successfully deployed the sample Spring Boot Application. </h1>";
